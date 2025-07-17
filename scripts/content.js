3215109348eb7c0b55f3aa1d3165a02ee948e735
karan51ngh <karansingh9535@gmail.com>
2025-07-17 23:55:15 +0530

    Button Creation

---
+++ scripts/content.js
@@ -0,0 +1,64 @@
+console.log("Hello Friend!")
+
+const observer = new MutationObserver(() => {
+    const footerActionsDiv = document.querySelector('[data-testid="composer-footer-actions"]');
+    const promptDiv = document.querySelector('#prompt-textarea');
+
+
+    if (footerActionsDiv && !document.getElementById('my-custom-button')) {
+
+        const improveButton = document.createElement('button');
+        improveButton.type = 'button';
+        improveButton.id = 'my-custom-button';
+        improveButton.className = 'composer-btn';
+        improveButton.setAttribute('aria-label', 'promptr-improve');
+        improveButton.innerHTML = `
+            <span class="ms-1.5 me-0.5">Improve ✨</span>`; 
+        improveButton.setAttribute('style', `
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            padding: 6px 10px;
+            background-color: transparent;
+            display: flex;
+            align-items: center;
+            gap: 4px;
+            cursor: pointer;`);
+
+        const ratingTab = document.createElement('button');
+        ratingTab.type = 'button';
+        ratingTab.id = 'my-custom-button';
+        ratingTab.className = 'composer-btn';
+        ratingTab.setAttribute('aria-label', 'promptr-rating');
+        ratingTab.innerHTML = `
+            <span class="ms-1.5 me-0.5">Rating</span>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="currentColor">
+                <circle cx="10" cy="10" r="6" />
+            </svg>`;
+        ratingTab.setAttribute('style', `
+            border: 1px solid #ccc;
+            border-radius: 8px;
+            padding: 6px 10px;
+            background-color: transparent;
+            display: flex;
+            align-items: center;
+            gap: 4px;
+            cursor: pointer;`);
+
+        improveButton.onclick = () => {
+            console.log('Clicked Improve button!')
+            const paragraphs = promptDiv?.querySelectorAll('p') || [];
+            const texts = Array.from(paragraphs).map(p => p.innerText);
+            console.log(texts);
+        };
+        ratingTab.onclick = () => console.log('Clicked Rating button!');
+
+        footerActionsDiv.appendChild(improveButton);
+        footerActionsDiv.appendChild(ratingTab);
+        observer.disconnect();
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
